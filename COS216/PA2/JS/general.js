e4b0a95ad4c8b8c9c33e3a2f0f090f9791d980be
Francois Smith <89078211+francois-smith@users.noreply.github.com>
2022-03-31 18:44:35 +0200

    Added JS to get Day for top of pages

---
+++ COS216/PA2/JS/general.js
@@ -0,0 +1,16 @@
+setupDate();
+
+function setupDate(){
+    const date = new Date();
+    let days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    let months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+
+    let weekDay = days[date.getDay()];
+    let day = date.getDate();
+    let month = months[date.getMonth()]
+    let year = date.getFullYear();
+
+    let dateSpan = document.getElementById("date-time").children[1];      
+
+    dateSpan.innerHTML = weekDay + ", " + day + " " + month + " " + year;
+}
