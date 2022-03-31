0b4894fd2ff2085b6e577b8c7f01437d1ccfef0f
Francois Smith <89078211+francois-smith@users.noreply.github.com>
2022-03-31 21:51:14 +0200

    SA news API implemented

--- COS216/PA2/JS/sa.js
+++ COS216/PA2/JS/sa.js
@@ -1,6 +1,63 @@
 getWeather();
+getNews();
 
-GET https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=glocations%3A(%22South%20Africa%22)&api-key=VUbv6VyQWcs3gVCvfI4abphAi5OkDhAt
+function getNews(){
+    const request = new XMLHttpRequest();
+
+    request.open("GET", "http://api.mediastack.com/v1/news?access_key=7c2da267305e87eeca21143f460ae7fb&languages=en&countries=za&sources=-News24&offset=2");
+    request.onload = () => {
+        let json = JSON.parse(request.responseText);
+        populateNews(json);
+    };
+    
+    request.send();
+}
+
+function populateNews(json){
+    console.log(json);
+
+    let data = json.data;
+    let goodArticles = [];
+
+    for(let article of data){
+        if(article.image != null){
+            goodArticles.push(article);
+        }
+    }
+
+    let articles = document.getElementsByClassName("general-latest-article");
+
+    let i = 0;
+    for(let article of articles){
+        article.querySelector("a").setAttribute("href", goodArticles[i].url);
+        article.querySelector(".latest-article-image").src = goodArticles[i].image;
+        article.querySelector(".latest-article-title").innerHTML = goodArticles[i].title;
+        article.querySelector(".latest-article-description").innerHTML = goodArticles[i].description.substring(0, 300);
+
+        let author = goodArticles[i].author;
+        if(author == null){
+            author = "Unknown Author";
+        }
+        let date = goodArticles[i].published_at;
+        article.querySelector(".latest-article-author").innerHTML = author + " - " + date.split('T')[0];  
+
+        let tag = article.querySelector(".tag");
+        tag.innerHTML = goodArticles[i].category;
+        tag.style.backgroundColor = tagGenerator();
+        
+        i++;
+    }
+
+    console.log(goodArticles);
+}
+
+function tagGenerator(){
+    var colors= ["#80B1D3", "#cc9c43", "#F47F71", "#8ED2C8", "#BC80B7", "#D9D9D8", "#F2AFD1", "#cac24c", "#BFBCDA"];
+
+    var i = Math.floor(Math.random()*colors.length);
+    return colors[i];
+}
+
 
 function getWeather(){
     const request = new XMLHttpRequest();
@@ -17,7 +74,6 @@ function getWeather(){
 }
 
 function populateWeather(json){
-    console.log(json);
     const types = ["Sunny", "Partly cloudy", "Moderate or heavy rain shower", "Clear", "Light rain", "Cloudy"];
 
     let weatherIcon = document.getElementById("weather-icon");
