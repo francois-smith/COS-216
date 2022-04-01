656c205fa5b8bf3cbe01f5a40f2fc968cbff5738
Francois Smith <89078211+francois-smith@users.noreply.github.com>
2022-04-02 01:07:32 +0200

    Calendar Fixed

--- COS216/PA2/JS/calendar.js
+++ COS216/PA2/JS/calendar.js
@@ -1,7 +1,19 @@
+let json;
 const date = new Date();
 
+function getNews(searchDate){
+	const request = new XMLHttpRequest();
+
+    request.open("GET", "http://api.mediastack.com/v1/news?limit=5&access_key=7c2da267305e87eeca21143f460ae7fb&sources=en&date="+searchDate);
+	request.onload = () => {
+        json = JSON.parse(request.responseText);
+    };
+
+	request.send();
+}
+
 const renderCalendar = () =>{ 
-    const monthDays = document.querySelector(".days");
+    const monthDays = document.querySelector(".days-of-month");
 
     const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
     const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
@@ -10,7 +22,6 @@ const renderCalendar = () =>{
     const lastDayIndex = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDay();
 
     const nextDays = 7 - lastDayIndex - 1;
-    console.log(lastDayIndex);
 
     const months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
 
@@ -26,10 +37,10 @@ const renderCalendar = () =>{
     for(let i = 1; i <= lastDay; i++){
         if(i === new Date().getDate() && date.getMonth() === new Date().getMonth() && date.getYear() === new Date().getYear())
         {
-            days += `<div class="selected">${i}</div>`;
+            days += `<div class="today" onclick="selectDate(this)">${i}</div>`;
         }
         else{
-            days += `<div>${i}</div>`;
+            days += `<div onclick="selectDate(this)">${i}</div>`;
         }
     }
 
@@ -51,3 +62,42 @@ document.querySelector(".next").addEventListener("click", () => {
 })
 
 renderCalendar();
+
+var article = document.querySelector(".article");
+var articleContainer = document.querySelector(".articles-container");
+
+let selectedDate = new Date();
+function selectDate(event){
+    const container = document.querySelector(".days-of-month");
+    const monthDays = container.querySelectorAll("div");
+    for(let day of monthDays){
+        day.classList.remove("selected");
+    }
+    event.classList.add("selected");
+    let selectedDate = new Date(date.getFullYear(), date.getMonth(), event.innerHTML);
+
+    let year = selectedDate.getFullYear();
+    let month = selectedDate.getMonth()+1;
+    let day = selectedDate.getDate();
+    let searchDate = year+"-"+month+"-"+day;
+    getNews(searchDate);
+
+    setTimeout(() => {
+        console.log(json);
+        articleContainer.innerHTML = "";
+        article.style.display = "flex";
+
+        for(let i = 0; i < 5; i++) {
+            const clone = article.cloneNode(true);
+            clone.querySelector(".article-date").innerHTML = (json.data[i].published_at).split("T")[0];
+            clone.querySelector(".article-title").innerHTML = json.data[i].title;
+            clone.querySelector(".link").setAttribute("href", json.data[i].url);
+            articleContainer.appendChild(clone);
+        }
+    }, 1000)
+}
+
+function goToLink(event){
+    var link = event.querySelector(".link").href;
+    window.open(link, '_blank').focus();
+}
