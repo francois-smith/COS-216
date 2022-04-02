1f61f3cb970b3d9ab0cd05b247e168a0eae84031
Francois Smith <89078211+francois-smith@users.noreply.github.com>
2022-04-02 21:54:08 +0200

    Updated API's

--- COS216/PA2/JS/covid.js
+++ COS216/PA2/JS/covid.js
@@ -1,70 +1,62 @@
-getCovidData();
+getGlobalData();
 
-function getCovidData(){
-	// selecting loading div
-    //const loader = document.querySelector("#loading-background");
-
-    //loader.classList.remove("display");
-    //loader.style.display = "none";
-
-    // setTimeout(() => {
-    //     loader.classList.remove("display");
-    // }, 3000);
-
-    // setTimeout(() => {
-    //     loader.style.display = "none";
-    // }, 4000);
-
-
-	const request1 = new XMLHttpRequest();
-    const request2 = new XMLHttpRequest();
-
-    request1.open("GET", "https://covid-19-statistics.p.rapidapi.com/reports?iso=ZAF");
-    request1.setRequestHeader("X-RapidAPI-Host", "covid-19-statistics.p.rapidapi.com");
-    request1.setRequestHeader("X-RapidAPI-Key", "d6c9483578msh8c34a069db0ddacp158131jsn2cb6e7ce986a");
-	request1.onload = () => {
-        let json = JSON.parse(request1.responseText);
-		populateSAData(json);
-    };
-
+function getGlobalData(){
+    const request = new XMLHttpRequest();
     
-    request2.open("GET", "https://covid-19-statistics.p.rapidapi.com/reports/total");
-    request2.setRequestHeader("X-RapidAPI-Host", "covid-19-statistics.p.rapidapi.com");
-    request2.setRequestHeader("X-RapidAPI-Key", "d6c9483578msh8c34a069db0ddacp158131jsn2cb6e7ce986a");
-    request2.onload = () => {
-        let json = JSON.parse(request2.responseText);
-        populateWorldData(json);
+    request.open("GET", "https://worldometers.p.rapidapi.com/api/coronavirus/all/");
+    request.setRequestHeader("X-RapidAPI-Host", "worldometers.p.rapidapi.com");
+    request.setRequestHeader("X-RapidAPI-Key", "d6c9483578msh8c34a069db0ddacp158131jsn2cb6e7ce986a");
+    request.onload = () => {
+        json = JSON.parse(request.responseText);
+        populateGlobalData(json);
+        populateSouthAfrica(json);
     };
 
-	request1.send();
-    request2.send();
+    request.send();
 }
 
-function populateSAData(json){
-    let data = json.data[0];
+function populateSouthAfrica(json){
+    let data;
+    for(let country of json.data){
+        if(country.Country == "SouthAfrica"){
+            data = country;
+            break;
+        }
+    }
 
+    console.log(data);
+
+    /*
     let casesContainer = document.querySelector(".cases-today");
     casesContainer.querySelector(".stat-amount").innerHTML = data.confirmed_diff;
 
     let deathsContainer = document.querySelector(".deaths-today");
     deathsContainer.querySelector(".stat-amount").innerHTML = data.deaths_diff;
+    */
 
-    let ifr = data.fatality_rate;
-    let ifrContainer = document.querySelector(".ifr");
-    ifrContainer.querySelector(".stat-amount").innerHTML = ifr.toPrecision(2) + "%";
+    // let ifr = data.fatality_rate;
+    // let ifrContainer = document.querySelector(".ifr");
+    // ifrContainer.querySelector(".stat-amount").innerHTML = ifr.toPrecision(2) + "%";
 
-    let cfr = (data.deaths/data.confirmed)*100;
-    let cfrContainer = document.querySelector(".cfr");
-    cfrContainer.querySelector(".stat-amount").innerHTML = cfr.toPrecision(2) + "%";    
+    // let cfr = (data.deaths/data.confirmed)*100;
+    // let cfrContainer = document.querySelector(".cfr");
+    // cfrContainer.querySelector(".stat-amount").innerHTML = cfr.toPrecision(2) + "%";    
 }
 
-function populateWorldData(json){
-    let data = json.data;
+function populateGlobalData(json){
+    let data = json.data[0];
+    console.log(json)
 
     let overallContainer = document.querySelector(".Overall-stats");
     let counters = overallContainer.getElementsByClassName("counter-amount");
+    let news = overallContainer.getElementsByClassName("counter-new");
 
-    counters[0].innerHTML =  data.confirmed.toLocaleString();
-    counters[1].innerHTML = (data.confirmed- 73029015).toLocaleString();
-    counters[2].innerHTML = data.deaths.toLocaleString();
+    counters[0].innerHTML =  data["Total Cases"];
+    counters[1].innerHTML = data["Total Recovered"];
+    counters[2].innerHTML = data["Total Deaths"];
+
+    news[0].innerHTML =  data["New Cases"];
+    news[1].innerHTML = data["New Recovered"];
+    news[2].innerHTML = data["New Deaths"];
+
 }
