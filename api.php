7797d632520dc5fcea50a7d3c43b77c0178f068a
Francois Smith <89078211+francois-smith@users.noreply.github.com>
2022-04-12 18:57:03 +0200

    API implementation, early stages

--- api.php
+++ api.php
@@ -1,3 +1,46 @@
 <?php 
+    include_once('COS216/PA3/php/General/config.php');
 
+    $database = Database::getInstance();
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if(!isset($_POST["type"])){
+            echo json_encode(failMessage("Type parameter must be set!"));
+            return;
+        }
+
+        $returnTypes = ["info", "update", "login", "rate", "chat"];
+        if(!in_array($_POST["type"], $returnTypes)){
+            echo json_encode(failMessage("Type not defined!"));
+            return;
+        }
+
+        if(!isset($_POST["return"])){
+            echo json_encode(failMessage("No return values specified!"));
+            return;
+        }
+
+        populateDatabase($database);
+        
+        //echo json_encode($_POST["return"]);
+    }    
+
+    function failMessage($message){
+        $date = date_create();
+        return ["status"=> "failed", "timestamp"=>date_timestamp_get($date), "data"=>["message"=>$message]];
+    }
+
+    function getArticle($link = "", $title = "", $author = "", $date = "", $image = "", $tag = "", $description = ""){
+        $date = date_create();
+        $returnString = ["status"=> "success", "timestamp"=>date_timestamp_get($date), "data"=>[]];
+        return $returnString;
+    }
+
+    function populateDatabase($database){
+        $q = "SELECT TOP 1 * FROM refreshed";
+        $r = $database->getConnection()->query($q);
+        $Date = strtotime($r);
+        //$NewDate = date($Date);
+        var_dump($Date);
+    }
 ?>
