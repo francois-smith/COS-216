780ac8e7b76fe93fb0b14496a59c1900b1ea23d3
Francois Smith <89078211+francois-smith@users.noreply.github.com>
2022-04-13 12:38:58 +0200

    API fully implemented(Still some bugs)

--- api.php
+++ api.php
@@ -1,37 +1,56 @@
 <?php
-
-use function PHPSTORM_META\type;
-
     include_once('COS216/PA3/php/General/config.php');
-
     $database = Database::getInstance();
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if(!isset($_POST["key"])){
+            echo json_encode(failMessage("Key parameter not set"));
+            return;
+        }
+
+        if(strlen($_POST["key"]) != 48){
+            echo json_encode(failMessage("Key parameter is invalid"));
+            return;
+        }
+
+
         if(!isset($_POST["type"])){
-            echo json_encode(failMessage("Type parameter must be set"));
+            echo json_encode(failMessage("Type parameter not set"));
             return;
         }
 
         $requestTypes = ["info", "update", "login", "rate", "chat"];
         if(!in_array($_POST["type"], $requestTypes)){
-            echo json_encode(failMessage("Type not defined"));
+            echo json_encode(failMessage("Type parameter not set"));
             return;
         }
 
         if(!isset($_POST["return"])){
-            echo json_encode(failMessage("No return values specified"));
+            echo json_encode(failMessage("No return parameters not set"));
             return;
         }
 
-        $returnTypes = ["title", "author", "date", "rating"];
+        $returnTypes = ["title", "description", "image", "author", "tag", "date", "rating", "link", "*"];
         if(empty(array_intersect($returnTypes, $_POST["return"]))){
-            echo json_encode(failMessage("Wrong return value specified"));
+            echo json_encode(failMessage("Invalid return parameter"));
             return;
         }
 
         switch ($_POST["type"]) {
             case "info":
-                getArticle($database);
+                if(!array_key_exists("title", $_POST)) $title = "";
+                else $title = $_POST["title"];
+
+                if(!array_key_exists("author", $_POST)) $author = "";
+                else $author = $_POST["author"];
+
+                if(!array_key_exists("date", $_POST)) $date = "";
+                else $date = $_POST["date"];
+
+                if(!array_key_exists("rating", $_POST)) $rating = "";
+                else $rating = $_POST["rating"];
+
+                getArticle($database, $title, $author, $date, $rating, $_POST["return"]);
                 break;
             case "update":
                 echo json_encode(failMessage("API type update does not exist"));
@@ -47,111 +66,98 @@ use function PHPSTORM_META\type;
                 break;
             default:
                 echo json_encode(failMessage("Unexpected error occured"));
-          } 
-
-        
-        //echo json_encode($_POST["return"]);
+        } 
     }    
 
     function failMessage($message){
         return ["status"=> "failed", "timestamp"=>time(), "data"=>["message"=>$message]];
     }
 
-    function getArticle($database, $link = "", $title = "", $author = "", $date = "", $image = "", $tag = "", $description = ""){
-        populateDatabase($database);
-        //$returnString = ["status"=> "success", "timestamp"=>time(), "data"=>[]];
-        //echo json_encode($returnString);
-    }
+    function getArticle($database, $title, $author, $date, $rating, $types){
+        $database->populateDatabase();
 
-    function populateDatabase($database){
-        $query = "SELECT `refreshed` FROM `refreshed` WHERE 1";
-        $result = $database->getConnection()->query($query);
+        $articles = [];
+        if($title != ""){
+            $query = "SELECT * FROM `articles` WHERE title LIKE '%$title%'";
+            $result = $database->getConnection()->query($query);
 
-        $value = null;
-        while($row = $result->fetch_assoc()){          
-            $value = $row["refreshed"];
+            $row = null;
+            while($row = $result->fetch_assoc()){          
+                $articles[] = $row;
+            }
         }
 
-        $refrshedTime = strtotime($value);
-        $currentTime = time();
+        if($author != ""){
+            $query = "SELECT * FROM `articles` WHERE author LIKE '%$author%'";
+            $result = $database->getConnection()->query($query);
 
-        $refresh = false;
-        if($currentTime - $refrshedTime >= 7200*3){
-            $refresh = true;
+            $row = null;
+            while($row = $result->fetch_assoc()){          
+                $articles[] = $row;
+            }
         }
 
-        if($refresh){
-            $worldURL = "https://api.nytimes.com/svc/news/v3/content/all/world.json?limit=10&api-key=D0YjNaMce336nUyLTHmot0vTCSFEUgdP";
-            $worldRequest = curl_init($worldURL);
-            curl_setopt($worldRequest, CURLOPT_URL, $worldURL);
-            curl_setopt($worldRequest, CURLOPT_RETURNTRANSFER, true);
-            $worldResponse = curl_exec($worldRequest);
-            $worldArticles = json_decode($worldResponse, true);
-            curl_close($worldRequest);
-            
+        if($date != ""){
+            $query = "SELECT * FROM `articles` WHERE `date` LIKE '%$date%'";
+            $result = $database->getConnection()->query($query);
 
-            $businessURL = "https://api.nytimes.com/svc/news/v3/content/all/business.json?limit=10&api-key=D0YjNaMce336nUyLTHmot0vTCSFEUgdP";
-            $businessRequest = curl_init($businessURL);
-            curl_setopt($businessRequest, CURLOPT_URL, $businessURL);
-            curl_setopt($businessRequest, CURLOPT_RETURNTRANSFER, true);
-            $businessResponse = curl_exec($businessRequest);
-            $businessArticles = json_decode($businessResponse, true);
-
-            $articles = array();
-            foreach($worldArticles["results"] as $article){
-                $articles[] = $article;
+            $row = null;
+            while($row = $result->fetch_assoc()){          
+                $articles[] = $row;
             }
-            foreach($businessArticles["results"] as $article){
-                $articles[] = $article;
-            }
-
-            //var_dump($articles["results"]);
-            /*
-
-            $url = "https://api.nytimes.com/svc/news/v3/content/all/health.json?limit=10&api-key=D0YjNaMce336nUyLTHmot0vTCSFEUgdP";
-            $request = curl_init($url);
-            $response = curl_exec($request);
-            $health = json_decode($response, true);
-
-            $url = "https://api.nytimes.com/svc/news/v3/content/all/movies.json?limit=10&api-key=D0YjNaMce336nUyLTHmot0vTCSFEUgdP";
-            $request = curl_init($url);
-            $response = curl_exec($request);
-            $movies = json_decode($response, true);
-
-            $url = "https://api.nytimes.com/svc/news/v3/content/all/science.json?limit=10&api-key=D0YjNaMce336nUyLTHmot0vTCSFEUgdP";
-            $request = curl_init($url);
-            $response = curl_exec($request);
-            $science = json_decode($response, true);
-
-            $url = "https://api.nytimes.com/svc/news/v3/content/all/sports.json?limit=10&api-key=D0YjNaMce336nUyLTHmot0vTCSFEUgdP";
-            $request = curl_init($url);
-            $response = curl_exec($request);
-            $sports = json_decode($response, true);
-
-            $url = "https://api.nytimes.com/svc/news/v3/content/all/technology.json?limit=10&api-key=D0YjNaMce336nUyLTHmot0vTCSFEUgdP";
-            $request = curl_init($url);
-            $response = curl_exec($request);
-            $technology = json_decode($response, true);
-
-            $url = "https://api.nytimes.com/svc/news/v3/content/all/travel.json?limit=10&api-key=D0YjNaMce336nUyLTHmot0vTCSFEUgdP";
-            $request = curl_init($url);
-            $response = curl_exec($request);
-            $travel = json_decode($response, true);
-
-            $articles = array();
-            foreach($worldArticles["results"] as $article){
-                $articles[] = $article;
-            }
-            foreach($businessArticles["results"] as $article){
-                $articles[] = $article;
-            }
-
-            var_dump($articles);
-
-            foreach ($articles as $article) {
-                
-            } 
-            */
         }
+
+        if($rating != ""){
+            $query = "SELECT * FROM `articles` WHERE `rating`='.$rating.'";
+            $result = $database->getConnection()->query($query);
+
+            $row = null;
+            while($row = $result->fetch_assoc()){          
+                $articles[] = $row;
+            }
+        }
+
+        if($title == "" && $date == "" && $author == "" && $rating == ""){
+            $query = "SELECT * FROM `articles` LIMIT 20";
+            $result = $database->getConnection()->query($query);
+
+            $row = null;
+            while($row = $result->fetch_assoc()){          
+                $articles[] = $row;
+            }
+        }
+
+        $correctTypes = ["title", "description", "image", "author", "tag", "date", "rating", "link", "*"];
+        $types = array_intersect($types, $correctTypes);
+
+        $data = [];
+        if(empty($articles)){
+            $returnString = ["status"=> "success", "timestamp"=>time(), "data"=>["data"=> "No articles found matching request criteria"]];
+            echo json_encode($returnString);
+            return;
+        }
+
+        $element = null;
+        $num = 0;
+        foreach($articles as $article){
+            if($num < 20){
+                foreach($types as $type){
+                    if($type == "*"){
+                        $element = $article;
+                        unset($element["id"]);
+                        break;
+                    }
+                    $element[$type] = $article[$type];
+                }
+                $data[] = $element;
+                $num++;
+            }
+            else{
+                break;
+            }
+        }
+        
+        $returnString = ["status"=> "success", "timestamp"=>time(), "data"=>$data];
+        echo json_encode($returnString);
     }
 ?>
