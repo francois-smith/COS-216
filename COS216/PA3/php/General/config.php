83dcb256144aed4b0278f913986e2a87f0609e20
Francois Smith <89078211+francois-smith@users.noreply.github.com>
2022-04-13 12:41:15 +0200

    starts session on construction(only once)

--- COS216/PA3/php/General/config.php
+++ COS216/PA3/php/General/config.php
@@ -1,5 +1,4 @@
 <?php
-    session_start();
 
     class Database{
         private static $instance = null;
@@ -11,7 +10,9 @@
         private $db = "users";
 
         private function __construct() {
+            session_start();
             $this->connection = new mysqli($this->servername, $this->username, $this->password, $this->db);
+            $this->populateDatabase();
         }
 
         public static function getInstance(){
@@ -46,13 +47,39 @@
                     $this->connection->query($sql);
                     $this->successMessage($api_key, $name);
                 } else {
-                    $this->failMessage("Email already registered");
+                    $this->failMessage($email);
                 }
             }
         }
 
-        private function failMessage($message){
+        public function addNews($title, $description, $author, $image, $tag, $articleDate, $link, $rating){
+            $sql1 = "INSERT INTO articles (title, description, author, image, tag, date, link, rating)";
+            $sql2 = " VALUES ('".$title."', '".$description."', '".$author."', '".$image."', '".$tag."', '".$articleDate."', '".$link."', '".$rating."')";
+            $this->connection->query($sql1.$sql2);
+        }
 
+        private function failMessage($email){
+            require_once("../General/header.php");
+            echo '
+                <head>
+                    <link rel="stylesheet" href="/COS216/PA3/CSS/signup.css">
+                </head>
+                <main>
+                    <div class="form-container">
+                        <div class="successContainer">
+                            <h2>An Error Occured</h2>
+                            <p>The following email is already registered: </p>
+                            <span id="email-container">'.$email.'</span>
+                            <p>Please sign in using this email</p>
+                        </div>
+                        <div class="quote-contianer">
+                            <p><span class="blue-text">Spreading </span><span>Knowledge</span></p>
+                        </div>
+                    </div>
+                </main>
+                <script src="/COS216/PA3/JS/signup.js"></script>
+            ';
+            require_once("../General/footer.php");
         }
 
         private function successMessage($message, $name){
@@ -76,6 +103,134 @@
                 <script src="/COS216/PA3/JS/signup.js"></script>
             ';
             require_once("../General/footer.php");
+        }
+
+        public function populateDatabase(){
+            $query = "SELECT `refreshed` FROM `refreshed` WHERE 1";
+            $result = $this->connection->query($query);
+    
+            $value = null;
+            while($row = $result->fetch_assoc()){          
+                $value = $row["refreshed"];
+            }
+    
+            $refrshedTime = strtotime($value);
+            $currentTime = time();
+    
+            $refresh = false;
+            if($currentTime - $refrshedTime >= 7200*3){
+                $refresh = true;
+            }
+    
+            if($refresh){
+                $url = "https://api.nytimes.com/svc/news/v3/content/all/world.json?limit=10&api-key=D0YjNaMce336nUyLTHmot0vTCSFEUgdP";
+                $request = curl_init($url);
+                curl_setopt($request, CURLOPT_URL, $url);
+                curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
+                $response = curl_exec($request);
+                $worldArticles = json_decode($response, true);
+    
+                $url = "https://api.nytimes.com/svc/news/v3/content/all/business.json?limit=10&api-key=D0YjNaMce336nUyLTHmot0vTCSFEUgdP";
+                $request = curl_init($url);
+                curl_setopt($request, CURLOPT_URL, $url);
+                curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
+                $response = curl_exec($request);
+                $businessArticles = json_decode($response, true);
+    
+                $url = "https://api.nytimes.com/svc/news/v3/content/all/health.json?limit=10&api-key=D0YjNaMce336nUyLTHmot0vTCSFEUgdP";
+                $request = curl_init($url);
+                curl_setopt($request, CURLOPT_URL, $url);
+                curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
+                $response = curl_exec($request);
+                $healthArticles = json_decode($response, true);
+    
+                $url = "https://api.nytimes.com/svc/news/v3/content/all/movies.json?limit=10&api-key=D0YjNaMce336nUyLTHmot0vTCSFEUgdP";
+                $request = curl_init($url);
+                curl_setopt($request, CURLOPT_URL, $url);
+                curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
+                $response = curl_exec($request);
+                $moviesArticles = json_decode($response, true);
+    
+                $url = "https://api.nytimes.com/svc/news/v3/content/all/science.json?limit=10&api-key=D0YjNaMce336nUyLTHmot0vTCSFEUgdP";
+                $request = curl_init($url);
+                curl_setopt($request, CURLOPT_URL, $url);
+                curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
+                $response = curl_exec($request);
+                $scienceArticles = json_decode($response, true);
+    
+                $url = "https://api.nytimes.com/svc/news/v3/content/all/sports.json?limit=10&api-key=D0YjNaMce336nUyLTHmot0vTCSFEUgdP";
+                $request = curl_init($url);
+                curl_setopt($request, CURLOPT_URL, $url);
+                curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
+                $response = curl_exec($request);
+                $sportsArticles = json_decode($response, true);
+    
+                $url = "https://api.nytimes.com/svc/news/v3/content/all/technology.json?limit=10&api-key=D0YjNaMce336nUyLTHmot0vTCSFEUgdP";
+                $request = curl_init($url);
+                curl_setopt($request, CURLOPT_URL, $url);
+                curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
+                $response = curl_exec($request);
+                $technologyArticles = json_decode($response, true);
+    
+                $url = "https://api.nytimes.com/svc/news/v3/content/all/travel.json?limit=10&api-key=D0YjNaMce336nUyLTHmot0vTCSFEUgdP";
+                $request = curl_init($url);
+                curl_setopt($request, CURLOPT_URL, $url);
+                curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
+                $response = curl_exec($request);
+                $travelArticles = json_decode($response, true);
+    
+                $articles = array();
+                foreach($worldArticles["results"] as $article){
+                    $articles[] = $article;
+                }
+                foreach($businessArticles["results"] as $article){
+                    $articles[] = $article;
+                }
+                foreach($healthArticles["results"] as $article){
+                    $articles[] = $article;
+                }
+                foreach($moviesArticles["results"] as $article){
+                    $articles[] = $article;
+                }
+                foreach($scienceArticles["results"] as $article){
+                    $articles[] = $article;
+                }
+                foreach($sportsArticles["results"] as $article){
+                    $articles[] = $article;
+                }
+                foreach($technologyArticles["results"] as $article){
+                    $articles[] = $article;
+                }
+                foreach($travelArticles["results"] as $article){
+                    $articles[] = $article;
+                }
+    
+                curl_close($request);
+    
+                foreach ($articles as $article) {
+                    if($article != null){
+                        if($article["multimedia"] != null){
+                            $description = addslashes($article["multimedia"][0]["caption"]);
+                            $image = addslashes($article["multimedia"][0]["url"]);
+                        }
+                        else{
+                            continue;
+                        }
+                        $title = addslashes($article["title"]);
+                        $author = addslashes($article["byline"]);
+                        $tag = addslashes($article["section"]);
+                        $date = addslashes($article["created_date"]);
+                        $link = addslashes($article["url"]);
+                        $rating = 0;
+    
+                        if($image != null){
+                            $this->addNews($title, $description, $author, $image, $tag, $date, $link, $rating);
+                        }
+                    }
+                } 
+                $sql = "UPDATE `refreshed` SET `refreshed`=CURRENT_TIMESTAMP";
+                $this->connection->query($sql);
+            }
         }
     }
 ?>
