ecb525d65afd01d948d59f02dd4708967024e3e2
Francois Smith <89078211+francois-smith@users.noreply.github.com>
2022-04-18 21:03:39 +0200

    Removed Filters

--- COS216/PA4/world.php
+++ COS216/PA4/world.php
@@ -6,12 +6,8 @@
     </div>
     <section class="general-news">
         <div class="general-header">
-            <h2 class="section-title">general</h2>
-            <div class="general-filters-container">
-                <span class="general-filter" onclick="generalFilter(this)"></span>
-            </div>
+            <h2 class="section-title">General</h2>
         </div>
-        
         <div class="general-container">
             <div onclick="goToLink(this)" class="general-article-main">
                 <a href="" class="link"></a>
@@ -55,68 +51,64 @@
             </div>
         </div>
     </section>
-    <section class="sports-news">
-        <div class="sports-header">
-            <h2 class="section-title">Sports</h2>
-            <div class="sports-filters-container">
-                <span class="sports-filter" onclick="sportFilter(this)"></span>
-            </div>
+    <section class="preferences-news">
+        <div class="preferences-header">
+            <h2 class="section-title">Preferences</h2>
         </div>
-        
-        <div class="sports-container">
-            <div onclick="goToLink(this)" class="main-sports-article">
+        <div class="preferences-container">
+            <div onclick="goToLink(this)" class="main-preferences-article">
                 <a href="" class="link"></a>
-                <img class="sport-article-image-main hide-alt" src="IMG/blank.png" alt="a">
-                <span class="tag sport"></span>
-                <div class="sport-article-content-main">
-                    <span class="sport-article-author-main"></span>
-                    <span class="sport-article-title-main">No Article Found</span>
-                    <span class="sport-article-description-main"></span>
+                <img class="preference-article-image-main hide-alt" src="IMG/blank.png" alt="a">
+                <span class="tag preference"></span>
+                <div class="preference-article-content-main">
+                    <span class="preference-article-author-main"></span>
+                    <span class="preference-article-title-main">No Article Found</span>
+                    <span class="preference-article-description-main"></span>
                 </div>      
             </div>
-            <div onclick="goToLink(this)" class="sport-sub-article1 sport-sub-article-general">
+            <div onclick="goToLink(this)" class="preference-sub-article1 preference-sub-article-general">
                 <a href="" class="link"></a>
-                <img class="sport-sub-article-image hide-alt" src="IMG/blank.png" alt="a">
-                <span class="tag sport"></span>
-                <div class="sport-sub-article-content">
-                    <span class="sport-sub-author-date"></span>
-                    <span class="sport-sub-article-title">No Article Found</span>
+                <img class="preference-sub-article-image hide-alt" src="IMG/blank.png" alt="a">
+                <span class="tag preference"></span>
+                <div class="preference-sub-article-content">
+                    <span class="preference-sub-author-date"></span>
+                    <span class="preference-sub-article-title">No Article Found</span>
                 </div> 
             </div>
-            <div onclick="goToLink(this)" class="sport-sub-article2 sport-sub-article-general">
+            <div onclick="goToLink(this)" class="preference-sub-article2 preference-sub-article-general">
                 <a href="" class="link"></a>
-                <img class="sport-sub-article-image hide-alt" src="IMG/blank.png" alt="a">
-                <span class="tag sport"></span>
-                <div class="sport-sub-article-content">
-                    <span class="sport-sub-author-date"></span>
-                    <span class="sport-sub-article-title">No Article Found</span>
+                <img class="preference-sub-article-image hide-alt" src="IMG/blank.png" alt="a">
+                <span class="tag preference"></span>
+                <div class="preference-sub-article-content">
+                    <span class="preference-sub-author-date"></span>
+                    <span class="preference-sub-article-title">No Article Found</span>
                 </div>
             </div>
-            <div onclick="goToLink(this)" class="sport-sub-article3 sport-sub-article-general">
+            <div onclick="goToLink(this)" class="preference-sub-article3 preference-sub-article-general">
                 <a href="" class="link"></a>
-                <img class="sport-sub-article-image hide-alt" src="IMG/blank.png" alt="a">
-                <span class="tag sport"></span>
-                <div class="sport-sub-article-content">
-                    <span class="sport-sub-author-date"></span>
-                    <span class="sport-sub-article-title">No Article Found</span>
+                <img class="preference-sub-article-image hide-alt" src="IMG/blank.png" alt="a">
+                <span class="tag preference"></span>
+                <div class="preference-sub-article-content">
+                    <span class="preference-sub-author-date"></span>
+                    <span class="preference-sub-article-title">No Article Found</span>
                 </div>
             </div>
-            <div onclick="goToLink(this)" class="sport-sub-article4 sport-sub-article-general">
+            <div onclick="goToLink(this)" class="preference-sub-article4 preference-sub-article-general">
                 <a href="" class="link"></a>
-                <img class="sport-sub-article-image hide-alt" src="IMG/blank.png" alt="a">
-                <span class="tag sport"></span>
-                <div class="sport-sub-article-content">
-                    <span class="sport-sub-author-date"></span>
-                    <span class="sport-sub-article-title">No Article Found</span>
+                <img class="preference-sub-article-image hide-alt" src="IMG/blank.png" alt="a">
+                <span class="tag preference"></span>
+                <div class="preference-sub-article-content">
+                    <span class="preference-sub-author-date"></span>
+                    <span class="preference-sub-article-title">No Article Found</span>
                 </div>
             </div>
-            <div onclick="goToLink(this)" class="sport-sub-article5 sport-sub-article-general">
+            <div onclick="goToLink(this)" class="preference-sub-article5 preference-sub-article-general">
                 <a href="" class="link"></a>
-                <img class="sport-sub-article-image hide-alt" src="IMG/blank.png" alt="a">
-                <span class="tag sport"></span>
-                <div class="sport-sub-article-content">
-                    <span class="sport-sub-author-date"></span>
-                    <span class="sport-sub-article-title">No Article Found</span>
+                <img class="preference-sub-article-image hide-alt" src="IMG/blank.png" alt="a">
+                <span class="tag preference"></span>
+                <div class="preference-sub-article-content">
+                    <span class="preference-sub-author-date"></span>
+                    <span class="preference-sub-article-title">No Article Found</span>
                 </div>
             </div>
             </div>
