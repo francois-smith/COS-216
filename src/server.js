4a546b3f7b3439f7fae1601ecacc0b97cad6259e
Francois Smith <89078211+francois-smith@users.noreply.github.com>
2022-04-22 20:03:42 +0200

    Socket Connections

--- src/server.js
+++ src/server.js
@@ -1,19 +1,37 @@
 const app = require("express")();
 const http = require("http").Server(app);
 const io = require('socket.io')(http);
+const api_url = "u21649988:Faffa0319!@wheatley.cs.up.ac.za/u21649988/api.php";
+var port = process.argv.slice(2);
+
+http.listen(port.toLocaleString(), function(){
+    console.log('listening on :'+port)
+});
 
 app.get('/', function(req, res){
-    res.sendfile('index.html');
+    let dir = __dirname;
+    dir = dir.replace("src", "");
+    res.sendFile(dir +'index.html');
 });
 
-io.on('connection', function(socket){
-    console.log('a user connected');
-
-    socket.on('disconnect', function(){
-        console.log('a user disconnected');
+let users = [];
+io.sockets.on('connection', function(socket){
+    socket.on('adduser', function(user){
+        socket.user = user;
+        users.push(user);
+        updateClients();
     });
-});
 
-http.listen(8000, function(){
-    console.log('listening on :8000')
+    socket.on('disconnect', function () {
+        for(var i=0; i<users.length; i++) {
+            if(users[i] == socket.user) {
+                users = users.splice(users[i], 1);
+            }
+        }
+        updateClients(); 
+    });
+
+    function updateClients() {
+        io.sockets.emit('update', users);
+    }
 });
