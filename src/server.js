f5cb9bea19e76a562084065d869d3e0603d9fdcd
Francois Smith <89078211+francois-smith@users.noreply.github.com>
2022-04-22 19:15:09 +0200

    Basic sockets

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.sendfile('index.html');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+
+    socket.on('disconnect', function(){
+        console.log('a user disconnected');
+    });
+});
+
+http.listen(8000, function(){
+    console.log('listening on :8000')
+});
