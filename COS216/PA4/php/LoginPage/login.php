0045432e040d97a90ba04f722d9a903c94fe4523
Francois Smith <89078211+francois-smith@users.noreply.github.com>
2022-04-22 18:31:44 +0200

    Login Form

---
+++ COS216/PA4/php/LoginPage/login.php
@@ -0,0 +1,31 @@
+<?php require_once("../General/header.php"); ?>
+<main>
+    <div class="form-container">
+        <div class="input-container">
+            <h2>Welcome Back</h2>
+            <form id="signup-form">
+                <div class="input-title-container">
+                    <label for="email">Email</label><br>
+                    <span class="error-message" id="email-error">Please enter a valid email address</span>
+                </div><br/>
+                <input type="email" id="email" name="email" required><br>
+                
+                <div class="input-title-container">
+                    <label for="email">Password</label><br>
+                    <span class="error-message" id="password-error">Please enter a valid password</span>
+                </div><br/>
+                <input type="password" id="password" name="password" required><img onclick="toggleIcon(this)" class="visibilty-toggle" src="/u21649988/COS216/PA4/img/SVG/invisible.svg" alt="none"><br>
+
+                <input type="submit" value="Log In">
+            </form>
+            <p>New User? <span onclick="registerPage()" id="register-link">Register</span></p>
+            <img id="logo-background" src="/u21649988/IMG/logo.svg" alt="logo">
+        </div>
+        <div class="quote-contianer">
+            <p><span class="blue-text">Spreading </span><span>Knowledge</span></p>
+        </div>
+    </div>
+</main>
+<script src="/u21649988/COS216/PA4/JS/login.js"></script>
+
+<?php require_once("../General/footer.php"); ?>
