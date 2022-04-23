cccf89d9db11f1361401c725ceb45b752c04527f
Francois Smith <89078211+francois-smith@users.noreply.github.com>
2022-04-23 22:24:39 +0200

    Toggle Theme Added

--- client/JS/general.js
+++ client/JS/general.js
@@ -1,5 +1,6 @@
 const port = ('; '+document.cookie).split(`; port_number=`).pop().split(';')[0];
 
+//checks what theme is active and swaps it for the other option
 function toggleTheme(){
     let theme = document.documentElement.className;
     if(theme == "light"){
@@ -14,3 +15,29 @@ function toggleTheme(){
     }
 }
 
+//passed in by successful user login, if a user logged in successfully thier saved theme will be loaded and applied to chat client
+function setTheme(theme){
+    if(theme == "light"){
+        document.querySelector("#light-theme").classList.add("display-none-theme");
+        document.querySelector("#dark-theme").classList.remove("display-none-theme");
+        document.documentElement.className = "light";
+    }
+
+    if(theme == "dark"){
+        document.querySelector("#dark-theme").classList.add("display-none-theme");
+        document.querySelector("#light-theme").classList.remove("display-none-theme");
+        document.documentElement.className = "dark";
+    }
+}
+
+//pulls login screen into frame to allow login
+function openLogin(){
+    let form = document.querySelector(".form-container");
+    form.style.transform = "translate(50%,-50%)";
+}
+
+//if cross on login sreen is pressed, hide the login screen from view
+function closeLogin(){
+    let form = document.querySelector(".form-container");
+    form.style.transform = "translate(200%, -50%)";
+}
