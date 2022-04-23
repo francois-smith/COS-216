7c04421e2a89102e6236bc4e7bfb08717ef08bac
Francois Smith <89078211+francois-smith@users.noreply.github.com>
2022-04-23 14:46:47 +0200

    Testing

--- client/JS/general.js
+++ client/JS/general.js
@@ -15,6 +15,26 @@ function toggleTheme(){
     }
 }
 
+$.ajax({
+    type: "POST",
+    'url': 'https://wheatley.cs.up.ac.za/u21649988/api.php',
+    dataType: "json",
+    data:{
+        "type":"login",
+        "email":"Test@gmail.com",
+        "password": "TestPass1!",
+        "return": ["*"]
+    },
+    username: 'u21649988',
+    password: 'Faffa0319',
+    error: function(xhr, status, error) {
+        alert(xhr.responseText);
+    },
+    success: function (res) {
+        console.log(jQuery.parseJSON(res));
+    }
+})
+
 function loginPage(){
     var request = new XMLHttpRequest();
     var path = "http://localhost:"+port+"/login";
