f80155fd93f3a311de6e5130602dfc52a9ddb803
Francois Smith <89078211+francois-smith@users.noreply.github.com>
2022-04-23 14:07:24 +0200

    Removed Redundant Functions

--- client/JS/general.js
+++ client/JS/general.js
@@ -1,38 +1,5 @@
-setupDate();
-
 const port = ('; '+document.cookie).split(`; port_number=`).pop().split(';')[0];
 console.log(port);
-
-function setupDate(){
-    const date = new Date();
-    let days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-    let months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
-
-    let weekDay = days[date.getDay()];
-    let day = date.getDate();
-    let month = months[date.getMonth()]
-    let year = date.getFullYear();
-
-    let dateSpan = document.getElementById("date-time").children[1];      
-
-    dateSpan.innerHTML = weekDay + ", " + day + " " + month + " " + year;
-}
-
-function getDate() {
-    var d = new Date(),
-    month = '' + (d.getMonth() + 1),
-    day = '' + d.getDate(),
-    year = d.getFullYear();
-
-    if (month.length < 2) {
-        month = '0' + month;
-    }
-    if (day.length < 2) {
-        day = '0' + day;
-    }
-
-    return [year, month, day].join('-');
-}
 
 function toggleTheme(){
     let theme = document.documentElement.className;
