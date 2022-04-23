fdb5b62d06cf5535443ca9b5770181e20dc15dc3
Francois Smith <89078211+francois-smith@users.noreply.github.com>
2022-04-23 22:24:51 +0200

    Updated Version

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
-//libraries
+//get required libraries from node_modules folder
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const { v1: uuidv1, v4: uuidv4 } = require('uuid');
 
 //setup express and sockets manager
 const app = express();
@@ -13,39 +14,43 @@ const username = 'u21649988';
 const password = 'Faffa0319';
 const auth = 'Basic ' + Buffer.from(username + ':' + password).toString('base64');
 
-//setup parsers
+//setup required parsers for handling cookies and POST data
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-//set directory to root
-let dir = __dirname;
-dir = dir.replace("src", "");
-
-//set port to listen to based on arguments
-let port = process.argv.slice(2);
-
-//make static library for client files
-app.use(express.static(dir + 'client'));
-
-//console log to display port and that setup was successful 
-http.listen(port.toLocaleString(), function(){
-    console.log('listening on :'+port)
-});
-
-//array to hold list of connected users
-let users = [];
-
-//set cookie for port on every user
+//create a cookie for every client that connects to allow dynamic ports to be accessible
+//put before client static library to allow cookies to be set
 app.get("/", (req, res) => {
+    res.cookie(`uuid`,uuidv4(), {httpOnly: false, sameSite: 'none', secure: true});
     res.cookie(`port_number`,port.toLocaleString(), {httpOnly: false, sameSite: 'none', secure: true});
     res.sendFile(dir, "client/index.html");
 });
 
-//when post comes in for login
+//set directiory root relative to server.js file
+let dir = __dirname;
+dir = dir.replace("src", "");
+
+//set port to listen to based on input arguments
+let port = process.argv.slice(2);
+
+//make static library for loading od client assets and files
+app.use(express.static(dir + 'client'));
+
+//setup port and log that server was setup correctly to desired port
+http.listen(port.toLocaleString(), function(){
+    console.log('listening on :'+port)
+});
+
+//array to hold a array of connected users on client
+let users = [];
+
+//when login menu submits a post is sent and caught by the function below
 app.post("/login", (req, res) => {
+    //setup required header
     const https = require('https');
     
+    //populate data to send to wheatley
     const data = JSON.stringify({
         type:"login",
         email: req.body.email,
@@ -53,9 +58,11 @@ app.post("/login", (req, res) => {
         return: ["*"]
     });
 
+    //setup wheatley location and connection path
     var hostName = "wheatley.cs.up.ac.za";
     var path = "/u21649988/api.php";
 
+    //setup post options for POSTing to Wheatley
     const options = {
         hostname: hostName,
         path: path,
@@ -67,34 +74,33 @@ app.post("/login", (req, res) => {
         }
     };
 
-    let returnString;
-    const request = https.request(options, (res) => {
-        res.on('data', (response) => {
-            returnString = response;
-            console.log(response.toString());
+    //run request, always return status 200(asuming Wheatley will recieve request)
+    //request is turned into JSON and passed back to client
+    const request = https.request(options, (returnData) => {
+        returnData.on('data', (response) => {
+            res.status(200).json(JSON.parse(response.toString()));
         });
     });
 
+    //if a error occured, log error to server console
     request.on('error', (error) => {
         console.log(error);
     });
 
+    //close request, resulting in response to client
     request.write(data);
-    request.end(data);
+    request.end();
 });
   
-app.get("/logout", (req, res) => {
-    // res.clearCookie("username");
-    // return res.redirect("/login");
-});
-
 //contains functions for user connections
 io.sockets.on('connection', function(socket){
-    socket.on('adduser', function(name){
-        //socket.user = user;
-        //users.push(user);
+    //function called by each user upon connection
+    socket.on('adduser', function(id){
+        socket.user = id;
+        users.push(id);
     });
 
+    //when user diconnects remove them from user array
     socket.on('disconnect', function () {
         for(var i=0; i<users.length; i++) {
             if(users[i] == socket.user) {
@@ -103,3 +109,7 @@ io.sockets.on('connection', function(socket){
         }
     });
 });
+
+function updateChat() {
+    io.sockets.emit('update', chatList);
+}
