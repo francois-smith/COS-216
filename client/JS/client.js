1e2d480eb7d0584d0a9344c9809504c6465f1316
Francois Smith <89078211+francois-smith@users.noreply.github.com>
2022-04-23 22:24:19 +0200

    Added Client Functions

--- client/JS/client.js
+++ client/JS/client.js
@@ -1,5 +1,12 @@
-const socket = io();
+const uuid = ('; '+document.cookie).split(`; uuid=`).pop().split(';')[0];
+console.log(uuid);
+const socket = io('http://localhost:'+port);
 
 socket.on('connect', function(){
-    socket.emit('adduser');
+    socket.emit('adduser', uuid);
+});
+
+socket.on('update', function (chatList){
+    
+
 });
