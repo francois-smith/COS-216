624f26d376bd5c4530a8e873cdc9acc5c90cefbf
Francois Smith <89078211+francois-smith@users.noreply.github.com>
2022-04-24 20:27:02 +0200

    Disconnect Popup added

--- client/JS/client.js
+++ client/JS/client.js
@@ -8,3 +8,25 @@ socket.on('connect', function(){
 socket.on('update', function (chatList){
     
 });
+
+socket.on('disconnect', function(){
+    toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": false,
+        "positionClass": "toast-bottom-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+
+    toastr["warning"]("You have been disconnected from server.", "Warning");
+});
