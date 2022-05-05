160895953e139beec1ede5551e535c3dd1eb25b4
Francois Smith <89078211+francois-smith@users.noreply.github.com>
2022-05-05 20:15:28 +0200

    Added Theme Toggle Code

--- COS216/PA4/JS/general.js
+++ COS216/PA4/JS/general.js
@@ -48,3 +48,17 @@ function openSettings(){
 function logOut(){
 
 }
+
+function toggleTheme(){
+    let theme = document.documentElement.className;
+    if(theme == "light"){
+        document.querySelector("#light-theme").classList.add("display-none-theme");
+        document.querySelector("#dark-theme").classList.remove("display-none-theme");
+        document.documentElement.className = "dark";
+    }
+    else{
+        document.querySelector("#dark-theme").classList.add("display-none-theme");
+        document.querySelector("#light-theme").classList.remove("display-none-theme");
+        document.documentElement.className = "light";
+    }
+}
